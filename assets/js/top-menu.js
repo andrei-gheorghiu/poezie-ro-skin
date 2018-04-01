4385a64b248fc450d9359fadb3a3271a90a1bb22
Andrei Gheorghiu <andrei.gheorghiu@kmap.io>
2018-04-01 05:07:57 +0100

    menu dropdowns

--- assets/js/top-menu.js
+++ assets/js/top-menu.js
@@ -1,55 +1,102 @@
 $(window).on('load', createMenu);
+$('#top-menu').on('click', '.dropdown .dropdown .dropdown-toggle', function(e) {
+  console.log('clicked!');
+  e.preventDefault();
+  e.stopImmediatePropagation();
+  let dd = $(e.target).closest('.dropdown');
+    dd.dropdown('toggle');
+    dd.parents('.dropdown').attr({
+      'aria-expanded':true
+    })
+});
 
+/**
+ *
+ */
 function createMenu() {
-    // you could get a json here to get it dynamically or...
-    let menu = [
-        'Poezie',
-        'Personale',
-        'Proză',
-        'Scenariu',
-        'Eseu',
-        'Presă',
-        'Articol',
-        'Comunități',
-        'Concurs',
-        'Special',
-        'Tehnică literară',
-        'Multimedia'
-    ];
-    let $ul = $('<ul>', {
-        class:'navbar-nav'
-    });
-    _.each(menu, (o,k)=> {
-        console.log(o,k);
-        $ul.append($('<li>',{
-            class:getItemClass(o, k),
-            html:getItemA(o)
-        }).append($('<nav-anim>')))
-    });
-    $('#top-menu').append($ul);
-}
+  let menu = getDefaultMenu();
+  let $ul = $('<ul>', {
+    class: 'navbar-nav'
+  });
+  _.each(menu, (o, k) => {
+    let li = $('<li>', {
+      class: getItemClass(o, k),
+      html: getItemA(o)
+    }).append($('<nav-anim>'));
+    $ul.append(li);
+    if (_.get(o, ['items', 'length'])) {
+      addSubmenu(o.items, li);
+    }
+  });
+  $('#top-menu').append($ul);
 
-function getItemClass(i, notFirst) {
-    let classes = 'nav-item' + (notFirst ? '' : ' active');
-    classes += _.isString(i) ? '' : _.get(i, ['items','length']) ? ' dropdown' :'';
-    return classes;
-}
+  function addSubmenu(items, parent) {
+    parent.addClass('dropdown');
+    parent.children().first().addClass('dropdown-toggle').attr({
+      "data-toggle": "dropdown",
+      "aria-haspopup": true,
+      "aria-expanded": false
+    });
+    let submenu = $('<ul>',{class:'dropdown-menu'});
+    _.each(items, (e, i) => {
+      let li = $('<li>', {
+        class: getItemClass(e, 1) + ' dropdown-item',
+        html: getItemA(e)
+      });
+      submenu.append(li);
+      /*if (_.get(e, ['items','length'])) {
+        addSubmenu(e.items, li);
+      }*/
+    });
+    parent.append(submenu);
+  }
 
-function getItemA(i) {
-    let tag = _.get(i,'tag') === 'span' ? $('<span>') : $('<a>');
-    tag.text(getItemTitle(i));
+  function getItemClass(item, notFirst) {
+    return 'nav-item' + (notFirst ? '' : ' active') +
+      (_.get(item, 'class') ? ' ' + item.class : '');
+  }
+
+  function getItemA(item) {
+    let tag = _.get(item, 'tag') === 'span' ? $('<span>') : $('<a>');
+    tag.text(getItemTitle(item));
     if (tag.is('a')) {
-        tag.attr('href', getItemLink(i))
+      tag.attr('href', getItemLink(item))
     }
     return tag;
-}
+  }
 
-function getItemLink(i) {
-    if (_.get(i,'href'))
-        return i.href;
-    return '';
-}
+  function getItemLink(item) {
 
-function getItemTitle(i) {
-    return _.isString(i) ? i : i.title || i.name;
+    return _.get(item, 'href') ? item.href : '';
+  }
+
+  function getItemTitle(i) {
+    return i ? _.isString(i) ? i : i.title || i.name || '' : '';
+  }
+
+  function getDefaultMenu() {
+    return [
+      'Poezie',
+      'Personale',
+      'Proză',
+      {
+        title: 'Scenariu',
+        items: [
+          'Acțiune',
+          'Comedie',
+          'Dramă',
+          'Scurt metraj',
+          'Teatru'
+        ]
+      },
+      'Eseu',
+      'Presă',
+      'Articol',
+      'Comunități',
+      'Concurs',
+      'Special',
+      'Tehnică literară',
+      'Multimedia'
+    ];
+  }
 }
